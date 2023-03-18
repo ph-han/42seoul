3149e95caab3f269179aa13c85f9a2db30b85cbd
Pilho Han <phan@c2r10s5.42seoul.kr>
2023-03-18 18:57:18 +0900

    add ft_memset.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: phan <phan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 21:49:28 by phan              #+#    #+#             */
-/*   Updated: 2023/03/18 18:03:12 by phan             ###   ########.fr       */
+/*   Updated: 2023/03/18 18:51:23 by phan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ int				ft_toupper(int c);
 int				ft_atoi(const char *str);
 int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
 void			ft_bzero(void *s, unsigned int n);
+void			*ft_memset(void *b, int c, unsigned int len);
 
 #endif
