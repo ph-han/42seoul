bf7136dd5c85f1489e1cc43f90706b5a8c0deb49
Pilho Han <phan@c2r10s5.42seoul.kr>
2023-03-18 19:00:36 +0900

    update value c converted to an unsigned char

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,20 +6,20 @@
 /*   By: phan <phan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 18:25:21 by phan              #+#    #+#             */
-/*   Updated: 2023/03/18 18:53:55 by phan             ###   ########.fr       */
+/*   Updated: 2023/03/18 18:59:18 by phan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memset(void *b, int c, unsigned int len)
 {
 	unsigned int	i;
-	char			*_b;
+	unsigned char	*_b;
 
-	_b = (char *)b;
+	_b = (unsigned char *)b;
 	i = 0;
 	while (i < len)
 	{
-		_b[i] = c;
+		_b[i] = (unsigned char)c;
 		i++;
 	}
 	return ((void *)_b);
