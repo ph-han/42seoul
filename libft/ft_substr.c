e9e68578d3a9d9463715f6734a038db899785910
Pilho Han <phan@c1r3s6.42seoul.kr>
2023-03-21 20:09:03 +0900

    update memory error in ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: phan <phan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 20:16:25 by phan              #+#    #+#             */
-/*   Updated: 2023/03/19 20:52:01 by phan             ###   ########.fr       */
+/*   Updated: 2023/03/21 20:07:22 by phan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*temp;
 	unsigned int	i;
-	
-	if (!s || ft_strlen(s) < start)
+
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s) < len)
+		temp = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	else
+		temp = (char *)malloc(sizeof(char) * (len + 1));
+	if (!temp)
 		return (0);
-	temp = (char *)malloc(sizeof(char) * (len + 1));
 	i = start;
-	while (*(s + i) && i < len + start)
+	while (i < start + len && *(s + i))
 	{
 		*(temp + (i - start)) = *(s + i);
 		i++;
 	}
-	*(temp + i) = '\0';
+	*(temp + (i - start)) = '\0';
 	return (temp);
 }
