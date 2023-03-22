345086de6a529f700eeda53fbdf4cf96bb3c28f8
Pilho Han <phan@c10r3s13.42seoul.kr>
2023-03-22 13:44:23 +0900

    update ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: phan <phan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 20:16:25 by phan              #+#    #+#             */
-/*   Updated: 2023/03/21 20:07:22 by phan             ###   ########.fr       */
+/*   Updated: 2023/03/22 13:42:46 by phan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*temp;
 	unsigned int	i;
+	unsigned int	temp_len;
 
-	if (ft_strlen(s) < start)
+	temp_len = ft_strlen(s);
+	if (temp_len < start)
 		len = 0;
-	if (ft_strlen(s) < len)
-		temp = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	else
-		temp = (char *)malloc(sizeof(char) * (len + 1));
+	temp_len -= start;
+	if (temp_len >= len)
+		temp_len = len;
+	temp = (char *)malloc(sizeof(char) * (temp_len + 1));
 	if (!temp)
 		return (0);
 	i = start;
