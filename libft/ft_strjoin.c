e8de1fd26dddc5e860c0bf3b92ed5da2218dbf4d
Pilho Han <phan@c10r3s13.42seoul.kr>
2023-03-22 20:48:58 +0900

    add ft_strjoin.c

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phan <phan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 20:14:43 by phan              #+#    #+#             */
+/*   Updated: 2023/03/22 20:48:26 by phan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*result;
+	unsigned int	s1_len;
+	unsigned int	s2_len;
+
+	if (!s1)
+		s1_len = 0;
+	if (!s2)
+		s2_len = 0;
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	result = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!result)
+		return (0);
+	ft_memmove(result, s1, s1_len);
+	ft_memmove(result + s1_len, s2, s2_len);
+	result[s1_len + s2_len] = '\0';
+	return (result);
+}
