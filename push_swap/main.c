2faa786a079db967e4a8f0196e3d81d6fec5788d
Pilho Han <phan@c1r4s6.42seoul.kr>
2023-05-24 20:59:35 +0900

    make sort 3 items in stack

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: phan <phan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:39:13 by phan              #+#    #+#             */
-/*   Updated: 2023/05/20 13:33:47 by phan             ###   ########.fr       */
+/*   Updated: 2023/05/24 20:49:16 by phan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,49 @@
 
 static void	push_swap(t_stack *a, t_stack *b)
 {
-	while (a->top)
+	if (a->size < 2)
+		return ;
+	if (a->top->item > a->top->next->item)
+		sa(a);
+	if (a->top->next->next)
 	{
-		ft_printf("a items : %d\n", top(a));
-		if (a->size == 0)
-			ft_printf("a stack is empty\n");
+		if (a->top->next->item > a->top->next->next->item)
+			rra(a);
+		if (a->top->item > a->top->next->item)
+			sa(a);
+		if (a->top->next->item > a->top->next->next->item)
+			rra(a);
 	}
+	while (a->top && b)
+	{
+		ft_printf("%d ", a->top->item);
+		a->top = a->top->next;
+	}
+	return ;
 }
+
+// static int	is_valid(char *item)
+// {
+
+// }
 
 int	main(int argc, char *argv[])
 {
 	t_stack	a;
 	t_stack	b;
-	int		item;
 	int		idx;
 
 	init(&a);
 	init(&b);
 	if (argc < 2)
+		return ((ft_printf("Error\n") * 0) + -1);
+	idx = argc - 1;
+	while (idx > 0)
 	{
-		ft_printf("Error\n");
-		return (0);
-	}
-	idx = 1;
-	while (idx < argc)
-	{
-		item = ft_atoi(argv[idx]);
-		if (item == 0 && *argv[idx] != '0')
-		{
-			ft_printf("Error\n");
-			return (0);
-		}
-		push(item, &a);
-		idx++;
+		// if (is_valid(argv[idx]) == 0)
+			// return ((ft_printf("Error\n") * 0) + -1);
+		push(ft_atoi(argv[idx]), &a);
+		idx--;
 	}
 	push_swap(&a, &b);
 	return (0);
