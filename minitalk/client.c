b99e53c57f9ebc720f121fff66257a7181c8591a
한필호 <hanpilho@hanpilhoui-MacBookAir.local>
2023-07-13 17:57:17 +0900

    ft_atoi update

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,30 +6,20 @@
 /*   By: phan <phan@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 20:43:46 by phan              #+#    #+#             */
-/*   Updated: 2023/07/13 11:41:33 by phan             ###   ########.fr       */
+/*   Updated: 2023/07/13 17:56:28 by phan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int sign;
-	long result;
+	long	result;
 
-	sign = 1;
 	result = 0;
-	while ((*str >= 9 && *str <= 13) || *str == 32)
-		str++;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
-		str++;
-	}
 	while (*str >= '0' && *str <= '9')
 		result = result * 10 + (*str++ - '0');
-	return ((int)(sign * result));
+	return ((int)(result));
 }
 
 void	send_bits(int pid, char c)
@@ -64,7 +54,7 @@ int	main(int ac, char *av[])
 		return (0);
 	}
 	pid = ft_atoi(av[1]);
-	if (pid < 0 || pid > 99998)
+	if (pid < 100 || pid > 99998)
 		return (0);
 	send_msg(pid, av[2]);
 }
