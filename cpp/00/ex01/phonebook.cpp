98caa4bf3e41b716bff2275c013fd9d7ab104e19
phan <hanfeelhoo@naver.com>
2023-11-01 23:30:15 +0900

    c00/ex01 start

---
+++ cpp/00/ex01/phonebook.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 한필호 on 11/1/23.
+//
+
+#include "phonebook.h"
+
+int main(void)
+{
+    return (0);
+}
---
+++ cpp/00/ex01/phonebook.h
@@ -0,0 +1,12 @@
+//
+// Created by 한필호 on 11/1/23.
+//
+
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+class PhoneBook {
+
+};
+
+#endif //PHONEBOOK_H
