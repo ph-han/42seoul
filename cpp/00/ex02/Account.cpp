ff507d818e91f1e06a923b9d2aaedfef68ebc7b2
phan <hanfeelhoo@naver.com>
2023-11-12 12:56:00 +0900

    ex02 ing..

--- cpp/00/ex02/Account.cpp
+++ cpp/00/ex02/Account.cpp
@@ -1,3 +1,14 @@
 //
 // Created by 한필호 on 11/11/23.
 //
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) : _nbDeposits(initial_deposit) {
+    _totalNbDeposits += initial_deposit
+}
+
+Account::displayAccountsInfos(void) {
+
+}
+
