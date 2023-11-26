18f03c5bac18ff8f450038856b0256ff340fe4a3
Pilho Han <phan@c1r5s1.42seoul.kr>
2023-11-26 19:13:30 +0900

    cpp01 starat

---
+++ cpp/01/ex00/Zombie.hpp
@@ -0,0 +1,17 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+private:
+	std::string name;
+public:
+	Zombie* newZombie( std::string name );
+	void	announce( void );
+	void	randomChump( std::string name );
+};
+
+#endif
---
+++ cpp/01/ex00/newZombie.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+Zombie* Zombie::newZombie(std::string name)
+{
+	Zombie zb;
+
+	
+	return (this);
+}
