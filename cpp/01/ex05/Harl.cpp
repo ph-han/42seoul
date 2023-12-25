4f9bcd81d311c088c158c17c4677a77ecc30ddb9
Pilho Han <phan@c1r4s7.42seoul.kr>
2023-12-25 14:00:20 +0900

    update

--- cpp/01/ex05/Harl.cpp
+++ cpp/01/ex05/Harl.cpp
@@ -19,13 +19,12 @@ void Harl::error(void) {
 }
 
 void Harl::complain(std::string level) {
-	Harl harl;
 	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	void (Harl::*funcs[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
 	for (int i = 0; i < 4; i++) {
 		if (levels[i] != level)
 			continue;
-		(harl.*funcs[i])();
+		(this->*funcs[i])();
 	}
 }
