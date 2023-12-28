e85fbbbd1d6e2dc3cf6234d2996b448c47cd31da
Pilho Han <phan@cx1r5s8.42seoul.kr>
2023-12-28 20:53:38 +0900

    make cpp02 ex03 folder

---
+++ cpp/02/ex03/Fixed.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+	private:
+		int					_fixedNumber;
+		static const int	_fractionalBits = 8;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &copy);
+		Fixed &operator=(const Fixed &obj);
+
+		Fixed(const int value);
+		Fixed(const float value);
+
+		Fixed operator+(const Fixed &obj);
+		Fixed operator-(const Fixed &obj);
+		Fixed operator*(const Fixed &obj);
+		Fixed operator/(const Fixed &obj);
+
+		Fixed &operator++();
+		Fixed &operator--();
+		Fixed operator++(int);
+		Fixed operator--(int);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		static Fixed	min(const Fixed& obj1, const Fixed& obj2);
+		static Fixed	max(const Fixed& obj1, const Fixed& obj2);
+};
+
+bool operator>(const Fixed& obj1, const Fixed& obj2);
+bool operator<(const Fixed& obj1, const Fixed& obj2);
+bool operator>=(const Fixed& obj1, const Fixed& obj2);
+bool operator<=(const Fixed& obj1, const Fixed& obj2);
+bool operator==(const Fixed& obj1, const Fixed& obj2);
+bool operator!=(const Fixed& obj1, const Fixed& obj2);
+
+std::ostream &operator<<(std::ostream &out, const Fixed &obj);
