13a5c2ab9d50cea413bcd2c3dc1f413aedb0dcc4
Pilho Han <phan@c1r6s3.42seoul.kr>
2024-01-05 15:37:30 +0900

    [fix] File open error & argument error

--- cpp/01/ex04/main.cpp
+++ cpp/01/ex04/main.cpp
@@ -6,13 +6,19 @@ int main(int ac, char **av) {
 	std::string filePath;
 	std::string replaceFile;
 
-	if (ac != 4)
+	if (ac != 4 || std::string(av[2]).length() < 1) {
+		std::cout << "argument error!" << std::endl;
 		return 0;
+	}
 
 	Sed sed(av[2], av[3]);
 	filePath = av[1];
 	replaceFile = filePath + ".replace";
 	std::ifstream openFile(filePath.data());
+	if (!openFile) {
+		std::cout << "File Open Error!" << std::endl;
+		return 0;
+	}
 	std::ofstream writeFile(replaceFile.data());
 	if (openFile.is_open() && writeFile.is_open()) {
 		std::string line;
