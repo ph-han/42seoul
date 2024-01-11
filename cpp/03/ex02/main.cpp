a7f5b3881fb00e89cf8cd3241adf62767e7e27dd
Pilho Han <phan@c1r6s6.42seoul.kr>
2024-01-11 14:27:27 +0900

    cpp03

--- cpp/03/ex02/main.cpp
+++ cpp/03/ex02/main.cpp
@@ -2,25 +2,25 @@
 #include "ScavTrap.hpp"
 
 int main(void) {
-	ClapTrap a("a");
-	ClapTrap b("b");
+	// ClapTrap a("a");
+	// ClapTrap b("b");
 
-	a.attack("b");
-	b.takeDamage(a.getAttackDamage());
-	b.beRepaired(a.getAttackDamage());
-	for (int i = 0; i < 10; i++)
-		a.attack("b");
+	// a.attack("b");
+	// b.takeDamage(a.getAttackDamage());
+	// b.beRepaired(a.getAttackDamage());
+	// for (int i = 0; i < 10; i++)
+	// 	a.attack("b");
 
-	ScavTrap c("c");
-	ScavTrap d("d");
+	// ScavTrap c("c");
+	// ScavTrap d("d");
 
-	c.attack("d");
-	d.takeDamage(c.getAttackDamage());
-	d.beRepaired(c.getAttackDamage());
-	for (int i = 0; i < 10; i++)
-		c.attack("d");
-	c.guardGate();
-	d.guardGate();
+	// c.attack("d");
+	// d.takeDamage(c.getAttackDamage());
+	// d.beRepaired(c.getAttackDamage());
+	// for (int i = 0; i < 10; i++)
+	// 	c.attack("d");
+	// c.guardGate();
+	// d.guardGate();
 
 	FragTrap e("e");
 	FragTrap f("f");
