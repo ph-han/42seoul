83541a6bbc4a97f655ff6b800e7f63235f6956ec
ph-han <phan@student.42seoul.kr>
2024-03-04 12:49:17 +0900

    cpp05 ex00 ok

--- cpp/05/ex00/Bureaucrat.hpp
+++ cpp/05/ex00/Bureaucrat.hpp
@@ -1,20 +1,44 @@
 #pragma once
 
 #include <iostream>
+#include <exception>
 
-class Bureaucrat {
+class Bureaucrat
+{
 	private:
 		const std::string _name;
-		int grade;
+		int _grade;
 	public:
 		Bureaucrat();
 		~Bureaucrat();
 		const Bureaucrat& operator=(const Bureaucrat& copy);
 		Bureaucrat(const Bureaucrat& copy);
+		Bureaucrat(std::string& name, int grade);
+
 
 		const std::string& getName(void) const;
 		int getGrade(void) const;
 
 		void increment(void);
-		void decrement(void);                                     
+		void decrement(void);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return "GradeTooHighException (range: 150 ~ 1)";
+				}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return "GradeTooLowException (range: 150 ~ 1)";
+				}
+		};
 };
+
+std::ostream& operator<<(std::ostream &out, const Bureaucrat &obj);
--- cpp/05/ex00/main.cpp
+++ cpp/05/ex00/main.cpp
@@ -2,5 +2,29 @@
 
 int main(void)
 {
+	try
+	{
+		// Bureaucrat a;
+		// std::cout << a << std::endl;
+		// a.decrement();
+		// std::cout << a << std::endl;
+		// a.increment();
+		// std::cout << a << std::endl;
+		// a.increment();
+		// std::cout << a << std::endl;
+
+		std::string name = "phan";
+		Bureaucrat b(name, 150);
+		std::cout << b << std::endl;
+		b.decrement();
+
+		// Bureaucrat c(name, 160);
+		// Bureaucrat d(name, 0);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 	return 0;
 }
