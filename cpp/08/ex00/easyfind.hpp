5a478f8d052cf059793ccb702aed1445b7f73103
ph-han <phan@student.42seoul.kr>
2024-03-27 19:39:33 +0900

    fix cpp08 ex00

--- cpp/08/ex00/easyfind.hpp
+++ cpp/08/ex00/easyfind.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
+#include <algorithm>
+
 template <typename T>
 typename T::iterator easyfind(T& container, int target)
 {
-	typename T::iterator iter = container.begin();
-
-	while (iter != container.end())
-	{
-		if (*iter == target)
-			break ;
-		iter++;
-	}
-
-	return iter;
+	return find(container.begin(), container.end(), target);
 }
