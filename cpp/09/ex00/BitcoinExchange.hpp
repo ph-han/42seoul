5b0b8ae70dd1f3817ae42395c85d7cc2c470c50b
ph-han <phan@student.42seoul.kr>
2024-04-01 17:46:59 +0900

    cpp09 start

---
+++ cpp/09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+class BitcoinExchange
+{
+	private:
+		
+	public:
+};
---
+++ cpp/09/ex00/main.cpp
@@ -0,0 +1,13 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cout << "Check your argument!" << std::endl;
+		return 1;
+	}
+
+	
+}
