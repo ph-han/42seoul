36424388995501d5d4bd1826eaf94a1d09929419
ph-han <phan@student.42seoul.kr>
2024-05-04 19:35:24 +0900

    change image name

--- inception/srcs/requirements/wordpress/tools/setup.sh
+++ inception/srcs/requirements/wordpress/tools/setup.sh
@@ -5,12 +5,11 @@ if [ ! -f /var/www/wordpress/index.php ]; then
 	wp core download  --locale=ko_KR
 	echo "Download ok"
 	wp config create  --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST
-	echo "config ok"
 	wp db create
-	echo "db created"
 	wp core install --url=phan.42.fr --title="Phan's Website" --admin_user=phan --admin_password=phan --admin_email=phan@wp-cli.org
 	wp user create hyuim hyuim@example.com --role=author --user_pass=1234
-	echo "wordpress ok"
+
+	chown -R nobody:nobody /var/www/wordpress 
 else
 	echo "Wordpress is already download."
 fi
