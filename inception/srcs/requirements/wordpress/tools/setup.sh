e1c192da1db9ebfea67b5c2920f0f080f1bf3b91
ph-han <phan@student.42seoul.kr>
2024-05-10 16:39:28 +0900

    fix: docker images tags, .env file

--- inception/srcs/requirements/wordpress/tools/setup.sh
+++ inception/srcs/requirements/wordpress/tools/setup.sh
@@ -6,8 +6,8 @@ if [ ! -f /var/www/wordpress/index.php ]; then
 	echo "Download ok"
 	wp config create  --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST
 	wp db create
-	wp core install --url=phan.42.fr --title="Phan's Website" --admin_user=phan --admin_password=phan --admin_email=phan@wp-cli.org
-	wp user create hyuim hyuim@example.com --role=author --user_pass=1234
+	wp core install --url=phan.42.fr --title="Phan's Website" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=phan@wp-cli.org
+	wp user create $WORDPRESS_USER hyuim@example.com --role=author --user_pass=$WORDPRESS_PASSWORD
 
 	chown -R nobody:nobody /var/www/wordpress 
 else
